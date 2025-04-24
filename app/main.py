db8167478253ba75303694196bf824ff6e1aae61
Arseni Fayeu <arsen13092005@gmail.com>
2025-04-24 23:53:59 +0200

    Create MVC prototype

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+
+import uvicorn
+
+import controller
+import model
+import view
+
+#Start FastAPI app
+app = FastAPI()
+
+#Root page
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return await controller.root_controller(request)
+
+#Run file if runfile named "main.py"
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
