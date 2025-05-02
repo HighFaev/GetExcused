b654a701b0eb65304efcdeeaf415ff316457eeeb
Arseni Fayeu <arsen13092005@gmail.com>
2025-05-02 20:27:35 +0200

    Added CORS settings

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 
 
@@ -10,7 +11,16 @@ import src.controllers.controller as controller
 #Start FastAPI app
 app = FastAPI()
 app.include_router(controller.router)
- 
+
+#Setup CORS settings
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],
+)
+
 
 #Run file if runfile named "main.py"
 if __name__ == "__main__":
